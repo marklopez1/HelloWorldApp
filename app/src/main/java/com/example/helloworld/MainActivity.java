3aebdfecb11ad36380782dbf7eaffde666735878
marklopez1 <markblopez@csu.fullerton.edu>
2020-02-16 13:25:26 -0800

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
+
+    }
+    private void goToSecondActivity()
+    {
+        Intent intent=new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
+    }
+
+}
+
---
+++ app/src/main/java/com/example/helloworld/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Button button2=(Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSettingsActivity();
+            }
+        });
+    }
+    private void goToSettingsActivity()
+    {
+        Intent intent2= new  Intent(this, SettingsActivity.class);
+        startActivity(intent2);
+
+    }
+}
